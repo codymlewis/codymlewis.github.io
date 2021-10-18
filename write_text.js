63b4f06c4e4e5d7542f63251336f6e435042914d
Cody Lewis <cody@codymlewis.com>
2021-10-18 20:25:02 +1100

    decided to follow some actual good practices

---
+++ write_text.js
@@ -0,0 +1,15 @@
+var titleText = "";
+var splitTitle = "Cody M. Lewis".split('');
+i = 0;
+var intervalId = setInterval(
+    () => {
+        titleText += splitTitle[i];
+        document.getElementById("video-overlay").innerText = titleText + "|";
+        i++;
+        if (i >= splitTitle.length) {
+            document.getElementById("video-overlay").innerText = titleText;
+            clearInterval(intervalId);
+        }
+    },
+    100
+)
